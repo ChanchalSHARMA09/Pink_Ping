1fe91988edf5bf282655cc469a04fbeaa24db8b4
chanchal <chanshar2021@gmail.com>
2026-02-15 11:30:30 +0530

    github account problem solving

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -7,6 +7,8 @@ import { signUp, signIn, logOut } from "../controllers/auth.controllers.js";
 
 const authRouter = express.Router();
 
+
+//api
 authRouter.post("/signup", signUp);
 authRouter.post("/signin", signIn);
 authRouter.get("/logout", logOut);
