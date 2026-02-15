9b9dbb8305c34d95f960afea6bd915c3b49024e0
chanchal <chanchal.2312051@gmail.com>
2026-02-15 11:34:38 +0530

    comment

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -8,7 +8,7 @@ import { signUp, signIn, logOut } from "../controllers/auth.controllers.js";
 const authRouter = express.Router();
 
 
-//api
+//api route
 authRouter.post("/signup", signUp);
 authRouter.post("/signin", signIn);
 authRouter.get("/logout", logOut);
